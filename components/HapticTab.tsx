3ba43a4d2d790ee61491a78f08e238e9ef02c0b4
oktayshakirov <oktayshakirov@gmail.com>
2025-07-17 01:24:37 +0200

    Selected tab state using navigation state

--- components/HapticTab.tsx
+++ components/HapticTab.tsx
@@ -3,6 +3,7 @@ import { BottomTabBarButtonProps } from "@react-navigation/bottom-tabs";
 import { PlatformPressable } from "@react-navigation/elements";
 import * as Haptics from "expo-haptics";
 import { useRefresh } from "@/contexts/RefreshContext";
+import { useNavigationState } from "@react-navigation/native";
 
 interface HapticTabProps extends BottomTabBarButtonProps {
   refreshKey: string;
@@ -12,8 +13,18 @@ export function HapticTab(props: HapticTabProps) {
   const { refreshKey, ...rest } = props;
   const { triggerRefresh } = useRefresh(refreshKey);
 
+  const currentRouteIndex = useNavigationState((state) => state?.index ?? 0);
+
+  const getTabIndex = (key: string) => {
+    const tabOrder = ["home", "posts", "exchanges", "ogs", "tools"];
+    return tabOrder.indexOf(key);
+  };
+
+  const tabIndex = getTabIndex(refreshKey);
+  const isSelected = tabIndex === currentRouteIndex;
+
   const handlePress = (ev: any) => {
-    if (props.accessibilityState?.selected) {
+    if (isSelected) {
       triggerRefresh();
     } else {
       props.onPress?.(ev);
